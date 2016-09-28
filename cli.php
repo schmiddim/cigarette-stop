1aaabbeb85a6156b0e983bde725229006b4075e4
ms <schmiddim@gmx.at>
2016-09-28 21:23:10 +0200

    hours

--- cli.php
+++ cli.php
@@ -1,18 +1,21 @@
 <?php
 /**
  *
- * example 	php cli.php 2016-09-19 30 0.285714286
+ * example    php cli.php 2016-09-19 30 0.285714286
  */
 
 if (4 !== $argc) {
-	echo 'usage: php '. __FILE__ . ' [date-stopped (Y-m-D)] [cigarettes per day] [price / cigarette]' . PHP_EOL;
+	echo 'usage: php ' . __FILE__ . ' [date-stopped (Y-m-D)] [cigarettes per day] [price / cigarette]' . PHP_EOL;
 	exit;
 }
 
-function getSmokingString(\DateTime $dateStopped, $cigarettesPerDay, $pricePerCigarette)
+function getSmokingString(\DateTime $dateStopped, $cigarettesPerDay=0, $pricePerCigarette=0)
 {
-	$daysWithoutSmoking = (new \DateTime('now'))->diff($dateStopped)->format('%a');
-	$cigarettesNotSmoked = $daysWithoutSmoking * $cigarettesPerDay;
+	$dateInterval = (new \DateTime('now'))->diff($dateStopped);
+	$daysWithoutSmoking =intval( $dateInterval->format('%a'));
+	$hoursToday = intval($dateInterval->format('%h'));
+	$cigarettesNotSmokedToday = round($cigarettesPerDay / 24 * $hoursToday,0);
+	$cigarettesNotSmoked = $daysWithoutSmoking * $cigarettesPerDay + $cigarettesNotSmokedToday;
 	$moneySaved = $pricePerCigarette * $cigarettesNotSmoked;
 	return sprintf('You stopped smoking %d days ago. Saved %d Euro, didnt\'t smoke %d cigarettes', $daysWithoutSmoking, $moneySaved, $cigarettesNotSmoked);
 }
@@ -23,3 +26,4 @@ $cigarettesPerDay = $argv[2];
 $pricePerCigarette = $argv[3];
 
 echo getSmokingString($dateStopped, $cigarettesPerDay, $pricePerCigarette);
+echo PHP_EOL;
